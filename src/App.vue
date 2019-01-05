9f897a7c4f02708f150b9dc603e55748aa66d12b
lxx.imac <lxx2013@mail.ustc.edu.cn>
2019-01-05 20:47:19 +0800

    初始化 App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,42 @@
 <template>
   <div id="app">
-    <Questions v-if="!isWaiting" :quiz="quiz" :count="count" :dies="dies" :chances="chances" :colors="colors"></Questions>
-    <water-back :percent="water" color="#1787ff" class="water-back"></water-back>
-    <Waiting v-if="isWaiting" keep-alive></Waiting>
+    <Waiting  v-show="is('waiting')"></Waiting>
+    <Question v-if="is('question')"></Question>
+    <Answer   v-if="is('answer')"></Answer>
+    <Score    v-if="is('score')"></Score>
   </div>
 </template>
 
 <script>
-import WaterBack from "./components/WaterBack";
-import Questions from "./components/questions";
-import Waiting from "./components/waiting";
-import socket from "./socket";
+import Question from "./components/question"
+import Waiting from "./components/waiting"
+import Answer from "./components/answer"
+import Score from "./components/score"
+import socket from "./socket"
 
 export default {
   name: "App",
   components: {
-    WaterBack,
     Questions,
-    Waiting
+    Waiting,
+    Answer,
+    Score
   },
   data() {
-    return {
-      quiz: {},
-      water: 20,
-      isWaiting: true,
-      timer: {},
-      count: [],
-      dies:[],
-      chances:2,
-      colors: { default: "rgba(255,255,255,0.92)", correct: "green", wrong: "rgba(255,255,255,0.5)" ,miss:"#efc4d3" },
-      name:"",
-    };
+    state:'waiting'
   },
   methods: {
-    wait() {
-      this.isWaiting = true;
-      this.setTimer(true)
-    },
-    /**
-     * 通过 this.timer 定时器 控制水流 this.water 的高度
-     * @param {boolean} isWaiting - true 时就上下波动,false 从下到上
-     * @param {number}  limit - 达到 limit 秒数后就 callback
-     */
-    setTimer(isWaiting, limit ,callback) {
-      clearInterval(this.timer);
-      if (isWaiting) {
-        var count = 0.08;
-        this.timer = setInterval(() => {
-          count += 0.02;
-          this.water = 20 + 5 * Math.sin(count);
-          if(limit && count >= (1000/33)*limit*0.02 ){
-            clearInterval(this.timer);
-            callback()
-          }
-        }, 33);
-      } else {
-        this.water = 0;
-        clearInterval(this.timer);
-        this.timer = setInterval(() => {
-          this.water += 102 / ((1000 / 33) * limit);
-          if (this.water >= 100) {
-            clearInterval(this.timer);
-            callback()
-          }
-        }, 33);
-      }
+    is(str){
+      return str == this.state
     }
   },
   computed: {},
   mounted() {
-    this.wait();
-    var that = this
-    that.name = "USER"+(Math.random()*200>>1)
-    socket.emit("client connected",that.name);
-    socket.on("server patchAnswer", data => {
-      Vue.set(this, "count", data.count);
-      that.dies = data.dies
-      that.isWaiting = false;
-      if(that.dies.some(x=>x.name == that.name)){
-        that.chances--
-        alert("你用掉了一次复活机会!剩余:"+that.chances)
-      }
-      //this.setTimer(true,10,this.wait)
-    });
-    socket.on("server wait", () => {
-      this.wait();
-    });
-    socket.on("server patchQuestion", singleQuestion => {
-      if (typeof navigator.vibrate === "function") navigator.vibrate(500);
-      Vue.set(this, "count", []);
-      this.quiz = singleQuestion;
-      this.isWaiting = false;
-      this.setTimer(false,15,this.wait)
-    });
+    socket.on('connectInfo',()=>{})
+    socket.on('question',()=>{})
+    socket.on('wait',()=>{})
+    socket.on('answer',()=>{})
+    socket.on('connectInfo',()=>{})
   }
 };
 </script>
@@ -112,9 +56,4 @@ export default {
   user-select none
 }
 
-.water-back {
-  z-index -1
-  height 100vh
-  overflow hidden
-}
 </style>
