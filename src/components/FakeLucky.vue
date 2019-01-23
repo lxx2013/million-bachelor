d103c865b7b5a440c481f480e96ce2e52230e657
laobubu <laobubu@live.cn>
2019-01-23 16:34:54 +0800

    抽奖样式调整，虽然还是很蠢

--- src/components/FakeLucky.vue
+++ src/components/FakeLucky.vue
@@ -1,12 +1,16 @@
 <template>
-  <WaterBack class="fakelucky" :percent="linePercent/3+50" color="#ff9">
+  <div class="fakelucky">
+    <h1 :class="{large:done}">幸运抽奖</h1>
+
     <div class="percentLine">
       <div class="fill" :style="`height:${linePercent}%`"></div>
     </div>
 
-    <div class="maybe guyCard" v-show="!done">
-      <img :src="current.avatar" class="avatar">
-      <p class="name">{{ current.name }}</p>
+    <div class="maybe" v-show="!done" :class="{freeze}">
+      <div class="guyCard">
+        <img :src="current.avatar" class="avatar">
+        <p class="name">{{ current.name }}</p>
+      </div>
     </div>
 
     <div class="confirmed">
@@ -15,7 +19,7 @@
         <p class="name">{{ winner.name }}</p>
       </div>
     </div>
-  </WaterBack>
+  </div>
 </template>
 
 <script>
@@ -36,6 +40,7 @@ export default {
 
   data() {
     return {
+      freeze: false,
       timer1: 0,
       timer2: 0,
       randIdx: 0,
@@ -56,6 +61,7 @@ export default {
     },
     /** @returns {ServerToWall.LuckyStart["players"][0]} */
     current() {
+      if (this.freeze) return this.winners[this.winnerCnt - 1];
       return this.players[this.randIdx];
     },
     /** @returns {ServerToWall.LuckyStart["winners"]} */
@@ -71,6 +77,8 @@ export default {
 
   methods: {
     roll() {
+      if (this.freeze) return;
+
       const ACCEL = 0.1;
       this.linePercent = (this.linePercent + 100 * ACCEL) / (ACCEL + 1);
 
@@ -78,11 +86,16 @@ export default {
         this.linePercent = 0;
         this.winnerCnt++;
 
-        if (this.winnerCnt == this.winners.length) {
-          clearInterval(this.timer1);
-          clearInterval(this.timer2);
-          this.done = true;
-        }
+        this.freeze = true;
+
+        setTimeout(() => {
+          this.freeze = false;
+          if (this.winnerCnt == this.winners.length) {
+            clearInterval(this.timer1);
+            clearInterval(this.timer2);
+            this.done = true;
+          }
+        }, 1000);
       }
     },
     roll2() {
@@ -95,12 +108,33 @@ export default {
 
 <style lang="stylus" scpoed>
 .fakelucky {
-  background: #aa3;
+  background-color: #F33;
+  background: linear-gradient(to bottom, #FA6, #F33);
   position: absolute;
   left: 0;
   top: 0;
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+}
+
+h1 {
+  text-align: center;
+  font-family: '微软雅黑';
+  color: #FFA;
+  text-shadow: 0 1px 4px #F33, 0 1px 4px #F33, 0 1px 10px #F33, 0 1px 24px #F33;
+  font-weight: bold;
+  letter-spacing: 5px;
+  font-size: 4vh;
+  transition: font-size 0.4s;
+  flex-shrink: 1;
+
+  &.large {
+    font-size: 10vh;
+    margin: 4vh 0;
+  }
 }
 
 .percentLine {
@@ -119,15 +153,15 @@ export default {
   }
 
   20% {
-    transform: translate(10px, -7px);
+    transform: translate(10px, -7px) rotate(5deg);
   }
 
   40% {
-    transform: translate(-8px, 3px);
+    transform: translate(-8px, 3px) scaleY(0.9);
   }
 
   60% {
-    transform: translate(-3px, 2px);
+    transform: translate(-3px, 2px) rotate(-5deg);
   }
 
   80% {
@@ -135,12 +169,36 @@ export default {
   }
 }
 
+@keyframes zoomin {
+  from {
+    transform: scale(1) translateY(-10%) scaleY(0.7);
+  }
+
+  80% {
+    transform: scale(1.4) translateY(6%);
+  }
+
+  to {
+    transform: scale(1.5);
+    text-shadow: 0 0 10px #FF9, 0 0 10px #FF9, 0 0 10px #FF9, 0 0 30px #FF9, 0 0 50px #FF9;
+  }
+}
+
 @keyframes boomin {
   0% {
+    transform: translateY(-200%);
     max-width: 0;
+    opacity 0
+  }
+
+  80% {
+    transform: translateY(20%);
+    max-width: 80vw;
+    opacity 0.7
   }
 
   100% {
+    transform: translateY(0);
     max-width: 100vw;
   }
 }
@@ -163,12 +221,29 @@ export default {
 }
 
 .maybe {
-  animation: shake 0.3s infinite;
+  display: flex;
+  justify-content: space-around;
+  z-index: 100;
+
+  .guyCard {
+    flex-grow: 0;
+    animation: shake 0.3s infinite;
+    z-index: 100;
+    filter: blur(1px);
+  }
+
+  &.freeze {
+    .guyCard {
+      animation: zoomin 0.5s 1;
+      animation-fill-mode: both;
+    }
+  }
 }
 
 .confirmed {
   display: flex;
   flex-wrap: wrap;
+  flex-grow: 1;
 
   .guyCard {
     text-shadow: 0 0 10px #FF9, 0 0 10px #FF9, 0 0 10px #FF9, 0 0 10px #FF9, 0 0 10px #FF9, 0 0 10px #FF9, 0 0 10px #FF9, 0 0 10px #FF9;
